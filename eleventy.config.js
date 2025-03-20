cff2f7420d75518232cf9f9a9c0baad1a77d9e4d
Josh <josh@joshthomas.dev>
2025-03-20 12:04:57 -0500

    Add asset hash plugin for cache busting and performance optimization

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,6 +2,7 @@ import EleventyPluginNavigation from "@11ty/eleventy-navigation";
 import EleventyPluginRss from "@11ty/eleventy-plugin-rss";
 import EleventyPluginSyntaxhighlight from "@11ty/eleventy-plugin-syntaxhighlight";
 import EleventyPluginFontAwesome from "@11ty/font-awesome";
+import EleventyPluginAssetHash from "@vrugtehagel/eleventy-asset-hash";
 import markdownIt from "markdown-it";
 import MarkdownItGitHubAlerts from "markdown-it-github-alerts";
 import collections from "./src/_config/collections.js";
@@ -22,6 +23,11 @@ export default function (eleventyConfig) {
   eleventyConfig.addBundle("html");
   eleventyConfig.addBundle("js");
 
+  eleventyConfig.addPlugin(EleventyPluginAssetHash, {
+    algorithm: "SHA-256",
+    include: ["**/*.html"],
+    includeAssets: ["**/*.{css,js}"],
+  });
   eleventyConfig.addPlugin(EleventyPluginFontAwesome);
   eleventyConfig.addPlugin(EleventyPluginNavigation);
   eleventyConfig.addPlugin(EleventyPluginRss);
